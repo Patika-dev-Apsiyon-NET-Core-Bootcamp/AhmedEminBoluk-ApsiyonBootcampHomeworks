9c35951c0e249a841b88ce898fa72fb3a25cc345
ahmedeminboluk <ahmedeminboluk@gmail.com>
2021-06-16 22:20:50 +0300

    TagHomeWork

---
+++ CleanArchitecture.Domain/CleanArchitecture.Application/Services/CategoryService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModel;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository = null;
+        private IMapper _mapper;
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> Add(CategoryViewModel category)
+        {
+            int id = _categoryRepository.Add(new Category { Name = category.Name, Description = category.Description, Status = category.Status, CreatedAt });
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CategoryViewModel> GetAll()
+        {
+            return _mapper.Map<List<CategoryViewModel>>(_categoryRepository.GetAll());
+        }
+
+        public CategoryViewModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(CategoryViewModel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CleanArchitecture.Domain/CleanArchitecture.Infrastructure.Data/Repositories/BlogRepository.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Domain.Models;
+using CleanArchitecture.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Data.Repositories
+{
+    public class BlogRepository : Repository<Blog>, IBlogRepository
+    {
+
+        public BlogRepository(BlogDbContext context) : base(context)
+        {
+
+        }
+
+        public async Task AddWithCategories(Blog blog)
+        {
+            _context.Categories.AttachRange(blog.Categories);
+            _context.Tags.AttachRange(blog.Tags);
+
+            await base.Add(blog);
+
+        }
+
+        public List<Blog> GetAllByCategory(int categoryId)
+        {
+
+            return _context.Blogs.Where(x => x.Categories.Any(y => y.Id == categoryId && y.Status)).ToList();
+        }
+
+        
+    }
+}
